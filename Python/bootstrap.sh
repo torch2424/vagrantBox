6abe62fb0891ff4f99a42a9057a950435080e112
Aaron Turner <aaron@aaronthedev.com>
2016-04-26 17:14:40 -0700

    Started a python vagrant for ML

---
+++ Python/bootstrap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#Made with help from Digital Ocean Guides
+#And my guide: https://github.com/torch2424/Elementary-Ubuntu-Web-Dev-Environment
+
+#Remove Non-interactive .bashrc lines
+sed '5,10d;' /home/vagrant/.bashrc > /home/vagrant/.bashrcNew
+mv /home/vagrant/.bashrcNew /home/vagrant/.bashrc
+
+#Update The Distro
+sudo apt-get update
+
+#Download things for Npm and Ruby(Compass and things)
+sudo apt-get install -y python-pip
+
+#Clone my bash-it and install
+git clone --depth=1 https://github.com/torch2424/bash-it.git ~/.bash_it
+~/.bash_it/install.sh < /vagrant/bashItInput.txt
+source /home/vagrant/.bashrc
+
+#Cache github credentials for 12 hours
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=43200'
+
+
+#Finished!
